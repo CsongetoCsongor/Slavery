d313ec6591b69272372a712f7cf811f0ef5220d3
sukebenedek <suke.benedek@students.jedlik.eu>
2025-11-18 12:52:17 +0100

    bench

--- tests/unit/slave.controller.test.js
+++ tests/unit/slave.controller.test.js
@@ -12,6 +12,33 @@ beforeEach(() => {
     next = null
 })
 
+// slave.controller.js
+
+const slaveModel = require("../model/slave.model");
+
+module.exports = {
+    getAllSlaves: async (req, res, next) => {
+        try {
+            const slaves = await slaveModel.find(); // Assuming `find()` fetches all slaves
+            res.status(200).json(slaves);
+        } catch (err) {
+            next(err);
+        }
+    },
+
+    getSlaveById: async (req, res, next) => {
+        try {
+            const slave = await slaveModel.findById(req.params.id); // Assuming `findById()` finds a slave by ID
+            if (!slave) {
+                return res.status(404).json({ message: "Slave not found" });
+            }
+            res.status(200).json(slave);
+        } catch (err) {
+            next(err);
+        }
+    }
+};
+
 
 
 
