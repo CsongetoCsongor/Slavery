27fa8eac66cb2c527b833353425812a69f0daeec
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2025-11-18 13:23:37 +0100

    delete unit test

--- tests/unit/slave.controller.test.js
+++ tests/unit/slave.controller.test.js
@@ -20,7 +20,38 @@ beforeEach(() => {
 
 
 
+slaveModel.findByIdAndDelete = jest.fn();
+describe('SlaveController.deleteSlave tests', () => {
+    it('should have a deleteSlave function', () => {
+        expect(typeof SlaveController.deleteSlave).toBe('function');
+    });
 
+    it('should call slaveModel.findByIdAndDelete with the correct ID', async () => {
+        req.params.id = '123';
+        await SlaveController.deleteSlave(req, res, next);
+        expect(slaveModel.findByIdAndDelete).toHaveBeenCalledWith('123');
+    });
+
+    it('should return 200 status code when slave is deleted successfully', async () => {
+        req.params.id = '123';
+        slaveModel.findByIdAndDelete.mockResolvedValue({ id: '123', name: 'John' });
+
+        await SlaveController.deleteSlave(req, res, next);
+
+        expect(res.statusCode).toBe(200);
+        expect(res._getJSONData()).toEqual({ message: 'Sikeresen törölve.' });
+    });
+
+    it('should return 404 when no slave is found with the provided ID', async () => {
+        req.params.id = '999';
+        slaveModel.findByIdAndDelete.mockResolvedValue(null);
+
+        await SlaveController.deleteSlave(req, res, next);
+
+        expect(res.statusCode).toBe(404);
+        expect(res._getJSONData()).toEqual({ message: 'Nem található ilyen ID-jű rabszolga.' });
+    });
+});
 
 
 
